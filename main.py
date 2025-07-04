f0ef684bb43086d7e89d628ea8c14af979aa26c1
Akarim <sambpapaabdoukarim@gmail.com>
2025-07-04 16:35:04 +0000

    Ajout de la fonction dire_aurevoir

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def dire_bonjour(nom):
     print(f"Bonjour, {nom} !")
 
+def dire_aurevoir(nom):
+    print(f"À bientôt, {nom} !")
+
 if __name__ == "__main__":
     nom = input("Ton nom : ")
     dire_bonjour(nom)
+    dire_aurevoir(nom)
